a845b4f499c831fd8c102404b831789324ee8c91
lucas1992 <lucas.etcheverry28@gmail.com>
2021-09-27 18:41:41 -0300

    Merge registración de empresas

--- src/app/service/config/config.service.ts
+++ src/app/service/config/config.service.ts
@@ -10,8 +10,8 @@ export class ConfigService {
 
   public static API_ENDPOINT() :string{
   
-    return "http://localhost:8000/";
-    //  return "https://api.maspedidos.com.ar/";
+    // return "http://localhost:8000/";
+    return "https://api.maspedidos.com.ar/";
   }
 
   public static DOMAIN() :string{
