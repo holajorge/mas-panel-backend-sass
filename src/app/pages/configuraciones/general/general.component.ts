5e8afcc5424f722ec252afd5a5050be0b36188e2
lucas1992 <lucas.etcheverry28@gmail.com>
2021-10-05 01:12:31 -0300

    Logo - Mobile

--- src/app/pages/configuraciones/general/general.component.ts
+++ src/app/pages/configuraciones/general/general.component.ts
@@ -17,6 +17,8 @@ export class GeneralComponent implements OnInit {
   empresaData:any = {id: ''};
   formConfig:any = [];
   configuraciones:any;
+  imageURLogo: string;
+  form_dataConfig:any=[];
 
   constructor(public translate: TranslateService,  
     public fb: FormBuilder, 
@@ -32,6 +34,8 @@ export class GeneralComponent implements OnInit {
       send_whatspp: [''],
       invitado: [null],
       empresa_id:  this.empresa,
+      logo_mobile: [null],
+      logo_mobile_file: [null],
     });
     this.empresaData.id = this.empresa;
   }
@@ -70,6 +74,11 @@ export class GeneralComponent implements OnInit {
     
     Swal.showLoading();
     let formData = new FormData();
+    if(Array.isArray(this.form_dataConfig)){}
+    else{
+      formData.append('logo_mobile_file', this.form_dataConfig.get('logo_mobile_file'));
+    }
+
     formData.append('aprobar_user',  this.formConfig.get('aprobar_usuario').value);
     formData.append('permitir_registracion',  this.formConfig.get('permitir_registracion').value);
     formData.append('send_whatspp',  this.formConfig.get('send_whatspp').value);
@@ -89,4 +98,48 @@ export class GeneralComponent implements OnInit {
     });
 
   }
+
+  showPreview(event) {
+    const file = (event.target as HTMLInputElement).files[0];
+    const files:FileList = event.target.files;
+    this.formConfig.patchValue({logo_mobile_file: file});
+    this.formConfig.get('logo_mobile_file').updateValueAndValidity();
+    if(files.length > 0){
+      const file = files[0];
+      if((file.size/1048576)<=4){
+        let formData = new FormData();
+        formData.append('logo_mobile_file', (event.target as HTMLInputElement).files[0], file.name);
+        this.form_dataConfig = formData;
+      }else{
+        Swal.fire('Error al importar o archivo excede o limite de tamaño permitido, intente de nuevo!', 'error');
+        return;
+      }
+    }
+
+    const reader = new FileReader();
+    reader.onload = () => {
+
+      var img = new Image();
+      img.src = reader.result as string;
+      img.onload = () => {
+          if(img.width != 512 || img.height != 512){
+            Swal.fire('Error al importar: la imagen debe ser de 512 x 512!', 'error');
+            this.deleteImage();
+            return;
+          }
+      }
+      this.imageURLogo = reader.result as string;
+    }
+    reader.readAsDataURL(file)
+  }
+
+  deleteImage(){
+    this.imageURLogo = "";
+    this.form_dataConfig.delete('logo_mobile_file');
+    this.formConfig.patchValue(
+      {logo_mobile:''}
+    );
+
+  }
+
 }
