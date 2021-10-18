7c2808e20a7472634ccfcf2551bdf6ea84325bbe
inmente <leandro@inmenteweb.com>
2021-10-18 01:15:12 -0300

    Íconos del menú

--- src/app/components/sidebar_admin/sidebar_admin.component.ts
+++ src/app/components/sidebar_admin/sidebar_admin.component.ts
@@ -34,7 +34,7 @@ export const ROUTES: RouteInfo[] = [
     path: "configuraciones",
     title: "Configuraciones",
     type: "sub",
-    icontype: "ni-map-big text-primary",
+    icontype: "fa fa-sliders-h text-primary",
     isCollapsed: true,
     children: [
       { path: "empresa", title: "Configuración de la empresa", type: "link" },
@@ -42,7 +42,7 @@ export const ROUTES: RouteInfo[] = [
       { path: "general", title: "Configuración general ", type: "link" },
     ]
   },
-  {
+ /* {
     path: "admin",
     title: "Dashboard",
     type: "sub",
@@ -51,17 +51,17 @@ export const ROUTES: RouteInfo[] = [
     children: [
       { path: "dashboard", title: "Dashboard", type: "link" },
     ]
-  },
+  }, */
   
   {
     path: "pedidos",
     title: "Pedidos",
     type: "sub",
-    icontype: "ni-shop text-primary",
+    icontype: "fa fa-shopping-cart text-primary",
     isCollapsed: true,
     children: [
       { path: "clientes", title: "Pedido de Clientes", type: "link" },
-      { path: "vendedores", title: "Pedido de Vendedores", type: "link" },    
+     /* { path: "vendedores", title: "Pedido de Vendedores", type: "link" }, */   
     ]
   },
   
@@ -69,7 +69,7 @@ export const ROUTES: RouteInfo[] = [
     path: "vendedor",
     title: "Vendedor",
     type: "sub",
-    icontype: "ni-shop text-primary",
+    icontype: "fa fa-user text-primary",
     isCollapsed: true,
     children: [
       { path: "vendedores", title: "Lista Vendedores", type: "link" },
@@ -82,7 +82,7 @@ export const ROUTES: RouteInfo[] = [
     path: "cliente",
     title: "Clientes",
     type: "sub",
-    icontype: "ni-shop text-primary",
+    icontype: "fa fa-user-friends text-primary",
     isCollapsed: true,
     children: [
       { path: "clientes", title: "Lista clientes", type: "link" },
@@ -101,7 +101,7 @@ export const ROUTES: RouteInfo[] = [
     path: "categorias",
     title: "Categorias",
     type: "sub",
-    icontype: "ni-shop text-primary",
+    icontype: "fa fa-folder text-primary",
     isCollapsed: true,
     children: [
       { path: "lista", title: "Lista Categoria", type: "link" },
@@ -112,7 +112,7 @@ export const ROUTES: RouteInfo[] = [
     path: "producto",
     title: "Productos",
     type: "sub",
-    icontype: "ni-shop text-primary",
+    icontype: "fa fa-cube text-primary",
     isCollapsed: true,
     children: [
       { path: "configuracion", title: "Configuración de productos", type: "link" },
@@ -125,11 +125,11 @@ export const ROUTES: RouteInfo[] = [
     path: "precios",
     title: "Precios", //Descuentos por Categorias
     type: "sub",
-    icontype: "ni-shop text-primary",
+    icontype: "fa fa-tags text-primary",
     isCollapsed: true,
     children: [
-      { path: "lista", title: "Lista Precios", type: "link" },
-      { path: "importar-precios", title: "Importar precios", type: "link" },
+     /* { path: "lista", title: "Lista Precios", type: "link" },
+      { path: "importar-precios", title: "Importar precios", type: "link" }, */
       { path: "descuento-lista", title: "Lista Descuentos", type: "link" },
       { path: "descuento-categorias", title: "Descuentos por Categorias", type: "link" },
     ]
@@ -138,7 +138,7 @@ export const ROUTES: RouteInfo[] = [
     path: "banners",
     title: "Banners",
     type: "link",
-    icontype: "ni-map-big text-primary",
+    icontype: "fa fa-images text-primary",
   },
 ];
 
