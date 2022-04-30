f873d85c3d2f266b1c38d4c39230f26f22da4126
franciscomigueljorge <franciscomigueljorge@gmail.com>
2022-04-30 11:45:08 -0500

    mas-318

--- src/app/pages/cliente/lista/lista.component.ts
+++ src/app/pages/cliente/lista/lista.component.ts
@@ -28,6 +28,8 @@ export class ListaComponent implements OnInit {
   entries: number = 10;
   activeRow:any;
   flagAdd:boolean = false;
+  nombreCliente:string = '';
+  fechaFilter:string;
   constructor(
     public translate: TranslateService,  
     public fb: FormBuilder,
@@ -67,6 +69,55 @@ export class ListaComponent implements OnInit {
       console.log(err);
     });
   }
+  esMayuscula(letra){
+    return letra === letra.toUpperCase();
+}
+comprobarTexto(itemBusqueda, textoInput ){
+
+  if(this.esMayuscula(textoInput)){
+    textoInput = textoInput.toUpperCase();
+    itemBusqueda = itemBusqueda.toUpperCase();
+    
+  }else{
+    textoInput = textoInput.toLowerCase()
+    itemBusqueda = itemBusqueda.toLowerCase();
+    
+  }
+  if(itemBusqueda.includes(textoInput)){
+    return true;
+  }else{
+    return false;
+  }
+
+}
+filters(){
+
+  const fecha = this.fechaFilter;
+  const nomCliente = this.nombreCliente;
+  console.log(fecha);
+  
+  const filtros = {
+    nombre: [nomCliente, d => {
+      if(this.comprobarTexto(d['nombre'],nomCliente)){        
+        return true;
+      }
+    }],
+    fechacomprobante: [fecha, d => {
+      let fechaData = d['fechacomprobante'].split(' ');
+      if(fechaData[0] == fecha){
+        return true;
+      }
+    }],
+
+  }
+  let comprobantes = this.tempRow;
+  for (const filtro in filtros) {
+    if(filtros[filtro][0]){
+      comprobantes = comprobantes.filter( filtros[filtro][1])
+    }
+  }
+  this.comprobantes = comprobantes;
+}
   delete(row){
 
     Swal.fire({
