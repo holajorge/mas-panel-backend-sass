418b6902833ee93aa9e61cf4544cc0cc2ad8531b
franciscomigueljorge <franciscomigueljorge@gmail.com>
2022-05-04 12:27:22 -0500

    acciones y filtro

--- src/app/pages/super-admin/empresas/empresas.component.ts
+++ src/app/pages/super-admin/empresas/empresas.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap';
 import { EmpresaService } from 'src/app/service/empresa/empresa.service';
 import Swal from 'sweetalert2';
 
@@ -9,7 +10,17 @@ import Swal from 'sweetalert2';
 })
 export class EmpresasComponent implements OnInit {
   empresas:any = [];
-  constructor(public empresaService:EmpresaService) { }
+  tempEmpresa:any = [];
+  nombreEmpresa:string = '';
+  empresa:string = '';
+  empresaDetails:[] = [];
+  notificationModal: BsModalRef;
+  notification = {
+    keyboard: true,
+    class: "modal-dialog-centered modal-xl static", 
+  };
+    
+    constructor(public empresaService:EmpresaService,private modalService: BsModalService) { }
 
   ngOnInit() {
     this.getEmpresas();
@@ -21,6 +32,7 @@ export class EmpresasComponent implements OnInit {
         Swal.close();
         console.log(datos);
         this.empresas = datos;
+        this.tempEmpresa = datos;
         console.log(this.empresas);
         
       },
@@ -33,5 +45,61 @@ export class EmpresasComponent implements OnInit {
   }
   newCompany(){
     
+  }
+  esMayuscula(letra){
+      return letra === letra.toUpperCase();
+  }
+  comprobarTexto(itemBusqueda, textoInput ){
+
+    if(this.esMayuscula(textoInput)){
+      textoInput = textoInput.toUpperCase();
+      itemBusqueda = itemBusqueda.toUpperCase();
+      
+    }else{
+      textoInput = textoInput.toLowerCase()
+      itemBusqueda = itemBusqueda.toLowerCase();
+      
+    }
+    if(itemBusqueda.includes(textoInput)){
+      return true;
+    }else{
+      return false;
+    }
+
+  }
+  filters(){
+
+    const nombre = this.nombreEmpresa;
+    
+    const filtros = {
+      
+      nombre: [nombre, d => {
+        if(this.comprobarTexto(d['nombre'],nombre)){
+          return true;
+        }
+      }],
+    }
+    let producto1 = this.tempEmpresa;
+    for (const filtro in filtros) {
+      if(filtros[filtro][0]){
+        producto1 = producto1.filter( filtros[filtro][1])
+      }
+    }
+    this.empresas = producto1;
+  }
+  eliminar(){
+    
+    this.nombreEmpresa = '';
+    this.empresas = this.tempEmpresa;
+  }
+
+  openInfo(modalEmpresa,empresa){
+    console.log(empresa);
+    this.empresa = empresa.nombre;
+    this.empresaDetails = empresa;
+    this.notificationModal = this.modalService.show(
+      modalEmpresa,
+      this.notification
+    );
   }
 }
