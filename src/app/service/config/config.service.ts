f688b38038acf32c7f78e5b02d036b021c3c6068
lucas1992 <lucas.etcheverry28@gmail.com>
2022-10-21 16:57:30 -0300

    MAS-412

--- src/app/service/config/config.service.ts
+++ src/app/service/config/config.service.ts
@@ -15,7 +15,7 @@ export class ConfigService {
   
     if(!environment.production){
 
-      return "http://localhost:8000/";
+      //return "http://localhost:8000/";
       return "https://api.maspedidos.com.ar/";
     }
     else{
@@ -80,8 +80,7 @@ export class ConfigService {
     });
   }
   saveGenerales(data){
-    return this._http.post(ConfigService.API_ENDPOINT()+"Backend/saveGenerales",data).toPromise().then((res) =>{     
-      console.log(data);
+    return this._http.post(ConfigService.API_ENDPOINT()+"Backend/saveGenerales",data).toPromise().then((res) =>{
       return { success: true, response:res};
     })
     .catch( (err) =>{
