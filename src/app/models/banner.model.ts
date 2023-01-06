002315b51eaefd1154fba16a9d164bfb4e3ede2b
zabaldo <davidaristizabalgiraldo@gmail.com>
2023-01-06 10:17:28 -0500

    Agregando opciones de fecha y link al modulo de banners

--- src/app/models/banner.model.ts
+++ src/app/models/banner.model.ts
@@ -3,6 +3,10 @@ export class Banner{
         public ruta:string,
         public movil:string,
         public tipo:string,
+        public fecha_desde: string,
+        public fecha_hasta: string,
+        public link: string,
+        public visible: string,
         public empresa_id:number,
         public id?:number,
     ){}
